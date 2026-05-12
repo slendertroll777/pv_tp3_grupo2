257acdfd56c5102d814c29a6a4cebfdc0ccd6758
Chosco Fabricio <fabrisiochosco38@gmail.com>
2026-05-12 18:45:15 -0300

    agregar arreglo inicial de proyectos al service Chosco

--- src/services/proyectoService.js
+++ src/services/proyectoService.js
@@ -1,3 +1,35 @@
 const proyectoService = (() => {
 
+let proyectos = [
+    {
+        id: 1,
+        titulo: "Sistema de Inscripciones",
+        categoria: "Educación",
+        estado: "En progreso"
+    },
+    {
+        id: 2,
+        titulo: "Campus Virtual",
+        categoria: "Web",
+        estado: "Completado"
+    },
+    {
+        id: 3,
+        titulo: "Gestión Académica",
+        categoria: "Administración",
+        estado: "Pendiente"
+    },
+    {
+        id: 4,
+        titulo: "Biblioteca Digital",
+        categoria: "Educación",
+        estado: "En progreso"
+    },
+    {
+        id: 5,
+        titulo: "Sistema de Tutorías",
+        categoria: "Académico",
+        estado: "Completado"
+    }
+];
 })();
