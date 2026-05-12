e2b056ea968b8e07f5035272b9216bb0ecee3ca4
franclyc <francoguanactolay61@gmail.com>
2026-05-12 14:50:22 -0300

    Agregando el Nav.jsx Guanactolay Franco

---
+++ src/components/nav.jsx
@@ -0,0 +1,12 @@
+import '/src/assets/css/nav.css';
+function Nav() {
+  return (
+    <nav>
+      <a href="#">Inicio</a>
+      <a href="#">Proyectos</a>
+      <a href="#">Perfil</a>
+    </nav>
+  );
+}
+
+export default Nav;
