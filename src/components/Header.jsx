44c156a4e523040ef99460f7ff7c0f87e514b8bb
valenan <valen.ang86@gmail.com>
2026-05-13 16:41:28 -0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
 import '/src/assets/css/header.css';
+=======
+<<<<<<< HEAD
+=======
+import '/src/assets/css/header.css';
+>>>>>>> angulo
+>>>>>>> Stashed changes
 const Header = () => {
   return (
     <header>
