3cd43143a9d8e5b03ca76dfdd98bad4b75d07af0
franclyc <francoguanactolay61@gmail.com>
2026-05-12 22:46:51 -0300

    importando el header al jsx Colque Inti

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Header from './components/Header.jsx';
+ <Header /> 
 import './App.css'
 
 function App() {
