f6a23589868c7a8b7c41498ac3a5ad8c9d5ffd42
Chosco Fabricio <fabrisiochosco38@gmail.com>
2026-05-20 17:36:52 -0300

    Agregar informacion visual y botones en proyectoCard Chosco

--- src/components/proyectoCard.jsx
+++ src/components/proyectoCard.jsx
@@ -1,12 +1,28 @@
 const ProyectoCard = ({ proyecto }) => {
 
+
+    const { titulo, categoria, estado, id } = proyecto;
+
     return (
-        <div>
+        <div className="proyecto-card">
+
+            <h2>{titulo}</h2>
+
+            <p>
+                <strong>Categoria:</strong> {categoria}
+            </p>
+
+            <p>
+                <strong>Estado:</strong> {estado}
+            </p>
+
+            <div className="botones-card">
+                <button>Eliminar</button>
+                <button>Ver detalle</button>
+            </div>
 
         </div>
     );
 };
 
 export default ProyectoCard;
-
-const { titulo, categoria, estado, id } = proyecto;
