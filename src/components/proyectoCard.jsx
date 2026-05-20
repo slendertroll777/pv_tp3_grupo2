57211b0cb56f894db518b4a0af4a6bda04cc7530
Chosco Fabricio <fabrisiochosco38@gmail.com>
2026-05-20 17:31:19 -0300

    Agregamos que pueda recibir promps y desestructuracion Chosco

---
+++ src/components/proyectoCard.jsx
@@ -0,0 +1,12 @@
+const ProyectoCard = ({ proyecto }) => {
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ProyectoCard;
+
+const { titulo, categoria, estado, id } = proyecto;
