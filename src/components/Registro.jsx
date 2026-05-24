52936534e2664a363c02813e932b9b218449b439
valenan <valen.ang86@gmail.com>
2026-05-24 00:42:35 -0300

    Estructura inicial Registro - Angulo

---
+++ src/components/Registro.jsx
@@ -0,0 +1,14 @@
+function RegistroActividad (fecha){
+    let dia = fecha.toLocaleDateString();
+    let hora = fecha.toLocaleTimeString();
+    let cambios = [];
+    let cambio = "Se registro un cambio a las " + hora + " del dia " + dia;
+    const agregarCambios = (nuevo) => {
+    };
+
+    return (
+        <div>
+
+        </div>
+    );
+};
