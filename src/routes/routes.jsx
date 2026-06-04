9a9095322f973aa9f977c9e4dc2351c261209445
franclyc <francoguanactolay61@gmail.com>
2026-06-03 21:01:34 -0300

    Agregado de rutas y componentes,Angulo Valentin y Guanactolay Franco

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Dashboard from "../views/Dahsboard";
+import Dashboard from "../views/Dashboard";
 import DetalleProyecto from "../views/DetalleProyecto";
 import PerfilUsuario from "../views/PerfilUsuario";
 import ErrorPage from "../views/ErrorPage";
+import ListaProyectos from "../components/ListaProyectos"; 
 
 const routes = createBrowserRouter([
   {
@@ -11,9 +12,24 @@ const routes = createBrowserRouter([
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <Dashboard /> },
-      { path: "detalle-proyecto", element: <DetalleProyecto /> },
-      { path: "perfil-usuario", element: <PerfilUsuario /> },
+      {
+        path: "dashboard",
+        element: <Dashboard />, 
+        children: [
+          {
+            index: true, 
+            element: <ListaProyectos />, 
+          },
+          {
+            path: "detalle-proyecto", 
+            element: <DetalleProyecto />,
+          },
+          {
+            path: "perfil-usuario", 
+            element: <PerfilUsuario />,
+          },
+        ],
+      },
     ],
   },
   {
