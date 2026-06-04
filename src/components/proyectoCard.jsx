4f8ceef17d653aae18aa37229adee5f9ebe75882
Inti Aragon <aragoncolqueinti6@gmail.com>
2026-06-04 00:48:45 -0300

    refactorizacion con bootstrap Chosco-Aragon

--- src/components/proyectoCard.jsx
+++ src/components/proyectoCard.jsx
@@ -1,43 +1,33 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-function ProyectoCard({ proyecto, onEliminar, onVerDetalle }) {
-    
+import { Link } from 'react-router-dom';
+
+function ProyectoCard({ proyecto, onEliminar }) {
     const { id, titulo, categoria, estado } = proyecto;
 
     return (
         <Card className="mb-3">
             <Card.Body>
-
                 <Card.Title>
                     {titulo}
                 </Card.Title>
-
                 <Card.Text>
                     <strong>Categoría:</strong> {categoria}
                 </Card.Text>
-
                 <Card.Text>
                     <strong>Estado:</strong> {estado}
                 </Card.Text>
-
                 <div className="d-flex gap-2">
-
-                    <Button
-                        variant="primary"
-                        onClick={onVerDetalle}
-                    >
+                    <Link to={`/proyectos/${id}`} className="btn btn-primary">
                         Ver Detalle
-                    </Button>
-
+                    </Link>
                     <Button
                         variant="danger"
                         onClick={() => onEliminar(id)}
                     >
                         Eliminar
                     </Button>
-
                 </div>
-
             </Card.Body>
         </Card>
     );
