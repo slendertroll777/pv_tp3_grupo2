aeba3d424c676fed9e987a5ae513765aaca4034d
Chosco Fabricio <fabrisiochosco38@gmail.com>
2026-06-04 23:14:15 -0300

    Usamos usuariocontext en header Chosco

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import '/src/assets/css/header.css';
 import Nav from "./Nav";
+import { useContext } from "react";
+import { UsuarioContext } from "../context/UsuarioContext";
 
 const Header = () => {
+  const { usuario } = useContext(UsuarioContext);
   return (
     <header>
       <h1>Gestión de Proyectos Educativos</h1>
+      <div>
+        {usuario.nombre} - {usuario.rol}
+      </div>
       <Nav />
     </header>
-
   );
 };
 export default Header;
