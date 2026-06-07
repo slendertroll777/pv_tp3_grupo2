ea2ae178e992c5765f5a447ed3a61acb2458df68
valenan <valen.ang86@gmail.com>
2026-06-06 21:01:29 -0300

    modificacion actividadCard - Angulo

--- src/components/actividadCard.jsx
+++ src/components/actividadCard.jsx
@@ -1,7 +1,10 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import '../services/usuarioService'
 
 function ActividadCard() {
+
+    
 
     return (
         <Card className="tarjeta-actividad" border="success" style={{ height: "100%" }}>
@@ -11,10 +14,10 @@ function ActividadCard() {
                         Estas son las últimas actividades registradas en la plataforma:
                     <ul>
                         <li>
-                            <strong>Usuarios activos: </strong> 2.
+                            <strong>Usuarios totales: </strong> 2.
                         </li>
                         <li>
-                            <strong>Proyectos activos: </strong> 3.
+                            <strong>Proyectos en progreso: </strong> 3.
                         </li>
                         <li>
                             <strong>Última actividad: </strong> 10 minutos atrás.
